9ed2c52ace5ed02fc9b1bd4432bf2b546686220b
Patrick James Louis <lumenfuzz@gmail.com>
2018-02-28 14:07:56 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -44,7 +44,7 @@ class TicTacToe
     index = 9
     until valid_move?(index)
       puts "Please enter 1-9:"
-      input = gets
+      input = @board.gets
       index = input_to_index(input)
     end
       move(index, current_player)
